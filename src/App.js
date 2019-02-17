6a0c572dde93719734fde1e9f78bdbbfd244ce79
Fahad Mostafa <fahadmustafa04@gmail.com>
2019-02-17 20:30:05 +0400

    added reactstrap library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -11,9 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <button className="btn btn-danger" type="button">
-          <h>Hello world</h>
-          </button>
+          <Button color="warning">Hello World</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
