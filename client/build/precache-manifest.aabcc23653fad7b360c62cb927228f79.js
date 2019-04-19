1d44a20d40687d08f0ebc1c490e9f3203b190316
Fahad Mostafa <fahadmustafa04@gmail.com>
2019-04-19 23:38:12 +0400

    changed a few things to make it build ready

---
+++ client/build/precache-manifest.aabcc23653fad7b360c62cb927228f79.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1740b4272675ee616cb1b6794df463a",
+    "url": "/static/media/flagWarningCooling.c1740b42.png"
+  },
+  {
+    "revision": "d79d1664e1ef8d415268",
+    "url": "/static/css/main.46946410.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25a1ba0ae4356880a8b7c96260aade18",
+    "url": "/static/media/Weather.25a1ba0a.png"
+  },
+  {
+    "revision": "d9368000d9956edfd317",
+    "url": "/static/js/2.2f4965ec.chunk.js"
+  },
+  {
+    "revision": "3a06ca1785ea01b4303075529bf80c6c",
+    "url": "/static/media/dsoa-logo-white.3a06ca17.png"
+  },
+  {
+    "revision": "7f891f6914b4fc1446b358e6315f7ee0",
+    "url": "/static/media/welcomebg.7f891f69.jpg"
+  },
+  {
+    "revision": "d79d1664e1ef8d415268",
+    "url": "/static/js/main.c6251bc0.chunk.js"
+  },
+  {
+    "revision": "3c3fd150141a82e880c18bef23db52e0",
+    "url": "/static/media/flagWarningMidday.3c3fd150.png"
+  },
+  {
+    "revision": "671c81aa45ca439894c8dfb54f7b08c7",
+    "url": "/static/media/flagWarningPuddle.671c81aa.png"
+  },
+  {
+    "revision": "b6d242a449b91584827d879a114176a2",
+    "url": "/static/media/flagWarningSummer.b6d242a4.png"
+  },
+  {
+    "revision": "1580169b1e07fe6010f7c8d95abf6901",
+    "url": "/static/media/flagWarningWater.1580169b.png"
+  },
+  {
+    "revision": "d9368000d9956edfd317",
+    "url": "/static/css/2.feafe1a9.chunk.css"
+  },
+  {
+    "revision": "77b953464a15d05315025c2ac1ccc456",
+    "url": "/index.html"
+  }
+];
--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const db = {}
-const sequelize = new Sequelize("weather_warning_system", "root", "", {
-    host: 'localhost',
+const sequelize = new Sequelize("iqcyly3uehp6y3sm", "oqphwv4y13dmwc2q", "j54bu4ujh3izf6yr", {
+    host: 'gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
     dialect: 'mysql',
     define: {
         timestamps: false
